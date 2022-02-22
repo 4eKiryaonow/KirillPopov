60ed018003a4fd28d9b38f734385a085c688f540
Kirill Popov <Kirill_Popov@epam.com>
2022-02-22 23:18:03 +0300

    created structure for hw4

---
+++ src/main/java/com/epam/tc/hw4/pageobject/component/HeaderPageComponent.java
@@ -0,0 +1,73 @@
+package com.epam.tc.hw4.pageobject.component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class HeaderPageComponent extends AbstractBaseComponent {
+
+    @FindBy(className = "navbar-right")
+    private WebElement dropDownProfileMenu;
+
+    @FindBy(id = "name")
+    private WebElement userName;
+
+    @FindBy(id = "password")
+    private WebElement passWord;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(id = "user-name")
+    private WebElement loggedUser;
+
+    @FindBy(css = ".m-l8 > li")
+    private List<WebElement> navigationMenu;
+
+    @FindBy(className = "dropdown")
+    private WebElement serviceDropdown;
+
+    public HeaderPageComponent(WebDriver driver) {
+
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+
+        dropDownProfileMenu.click();
+        userName.sendKeys(username);
+        passWord.sendKeys(password);
+        loginButton.click();
+    }
+
+    public WebElement getLoggedUser() {
+
+        return loggedUser;
+    }
+
+    public List<String> getTitlesNavigationMenu() {
+
+        return navigationMenu
+            .stream()
+            .map(WebElement::getText)
+            .collect(Collectors.toList());
+    }
+
+    public void clickByServiceDropDownMenu() {
+
+        wait.until(ExpectedConditions.elementToBeClickable(serviceDropdown))
+            .click();
+    }
+
+    public void clickByDifferentElements() {
+
+        this.clickByServiceDropDownMenu();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//ul[contains(@class, 'm-l8')]/descendant::ul/descendant::*[text()='Different elements']")))
+            .click();
+    }
+}
---
+++ src/test/java/com/epam/tc/hw4/PropertiesReader.java
@@ -0,0 +1,25 @@
+package com.epam.tc.hw4;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader {
+
+    protected static Properties PROPERTIES;
+
+    static {
+        try (FileInputStream fileInputStream = new FileInputStream("src/test/resources/config.properties")) {
+
+            PROPERTIES = new Properties();
+            PROPERTIES.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+}
