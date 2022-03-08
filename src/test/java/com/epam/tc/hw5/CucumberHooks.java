22564fdb0aa8d91cb11fce7c2278dbbe64310e1e
Kirill Popov <Kirill_Popov@epam.com>
2022-03-08 17:19:39 +0300

    fix pom and CucumberHooks files

--- src/test/java/com/epam/tc/hw5/CucumberHooks.java
+++ src/test/java/com/epam/tc/hw5/CucumberHooks.java
@@ -7,19 +7,25 @@ import io.cucumber.java.BeforeAll;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CucumberHooks {
 
     private WebDriver driver;
+    private ChromeOptions options;
 
     @BeforeAll
     public static void setUpAll() {
         WebDriverManager.chromedriver().setup();
+
     }
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
+        options = new ChromeOptions()
+            .addArguments("start-maximized",
+                "-disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         TestContext.getInstance().setObject("driver", driver);
     }
 
